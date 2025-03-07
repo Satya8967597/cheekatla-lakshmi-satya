74196eae1355cb6ba396621b7302234c1794d2c0
Satya8967597 <lakshmipalla42@gmail.com>
2025-03-07 23:49:11 +0530

    Add files via upload

---
+++ betweennumberswhile.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class betweennumberswhile {
+    public static void main(String[] args) {
+
+        int i = 1;
+        do {
+            System.out.print(i + " ");
+            i++;
+        }
+        while (i <= 10);
+    }
+}
+    
+
---
+++ ten.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class ten {
+    public static void main(String[] args) {
+        String x="Bright IT career";
+        for(int i=0;i<10;i++)
+        {
+            System.out.println(x);
+        }
+    }
+    
+}
